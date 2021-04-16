c8b7bc63b288f3e7f2a1007e1776cd029bcf14a9
Mr.Billy <80222544+jirapat252262120@users.noreply.github.com>
2021-04-16 17:03:28 +0700

    sd

---
+++ work_off/rent_build/view_rent.php
@@ -0,0 +1,9 @@
+<div class="card">
+    <div class="card-header text-center font-weight-bold">
+        <h3>ข้อมูลการเช่าอาคาร</h3>
+    </div>
+    <div class="card-body">
+     <!-- แก้ กง งี้ -->
+     <text>ตัวอย่าง :</text> <text>Value</text>
+    </div>
+</div>
